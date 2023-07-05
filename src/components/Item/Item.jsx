e6a5dc7faab95432ff6a99bb1cf50e29c216c383
cecilopezbernal <cecilopezbernal@gmail.com>
2023-07-05 13:18:13 +0200

    segunda entrega

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,7 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
-const Item = ({ nombre, text, precio, img }) => {
+const Item = ({ id, nombre, text, precio, img }) => {
     return (
 
         <Card style={{ width: '18rem' }}>
@@ -10,7 +11,7 @@ const Item = ({ nombre, text, precio, img }) => {
                 <Card.Title>{nombre}</Card.Title>
                 <Card.Subtitle>{precio} €</Card.Subtitle>
                 <Card.Text>{text}</Card.Text>
-                <Button variant="primary"> más info </Button>
+                <Link to={`/item/${id}`}><Button variant="primary"> más info </Button></Link> 
             </Card.Body>
         </Card>
 
